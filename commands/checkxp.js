27564cd552a463b540c66d11b129d7a6be83a40b
SynnDC <73966450+bacon1177@users.noreply.github.com>
2023-11-13 19:11:02 -0800

    checkxp.js

--- commands/checkxp.js
+++ commands/checkxp.js
@@ -3,17 +3,174 @@ const { Client, EmbedBuilder, CommandInteraction, ActionRow, ButtonInteraction }
 const { GroupId } = require('../config.json')
 const Roblox = require('noblox.js');
 
-async function fetchXP(userId) {
-    try {
-        console.log(`Fetching XP data for user with userId: ${userId}`);
-        const xp = await Roblox.getData({ target: userId, key: 'XP' });
-        console.log(`Fetched XP data for user with userId ${userId}: ${xp}`);
-        return xp;
-    } catch (error) {
-        console.error('Error fetching XP data:', error);
-        throw new Error('Failed to fetch XP data.');
-    }
+const express = require('express');
+var bodyParser = require('body-parser');
+const https = require('https');
+const app = express();
+
+const Invalid = new MessageEmbed()
+  .setColor('#eb4034')
+  .setDescription("Invalid user")
+
+var toGet = [];
+function byUID(method,usr,message) {
+  const Emb = new MessageEmbed()
+      .setColor('#fff200')
+      //.setTitle(request.headers.username + "'s Data")
+      //.setTitle("Attempt")
+      //.setAuthor('Roblox Error','')
+      .setDescription("Attempting to "+method+" UserID "+ usr +"...")
+      .setTimestamp()
+      .setFooter('Developed by Stratiz');
+    message.edit(Emb);
+  https.get("https://api.roblox.com/users/" + usr, (res) => {
+      
+      let data = '';
+      res.on('data', d => {
+        data += d
+      })
+      res.on('end', () => {
+        if (res.statusCode == 200) {
+          toGet.push({method: method,username: JSON.parse(data).Username,value: usr,cid: message.channel.id,mid: message.id});
+        } else {
+          message.edit(Invalid);
+        }
+      });
+  }).on('error', error => {
+    console.error("RBLX API (UID) | " + error);
+  });
 }
+
+function byUser(method,usr,message) {
+    const Emb = new MessageEmbed()
+          .setColor('#fff200')
+          //.setTitle(request.headers.username + "'s Data")
+          //.setTitle("Attempt")
+          //.setAuthor('Roblox Error','')
+          .setDescription("Attempting to "+method+" username "+ usr +"...")
+          .setTimestamp()
+          .setFooter('Developed by Stratiz');
+    message.edit(Emb);
+    https.get("https://api.roblox.com/users/get-by-username?username=" + usr, (res) => {
+        let data = '';
+        res.on('data', d => {
+          data += d
+        })
+        res.on('end', () => {
+          if (JSON.parse(data).Id != undefined) {
+            toGet.push({method: method,value: JSON.parse(data).Id,username: JSON.parse(data).Username,cid: message.channel.id,mid: message.id});
+          } else {
+            message.edit(Invalid);
+          }
+        });
+    }).on('error', error => {
+      console.error("RBLX API (Username) | " + error);
+    });
+}
+
+const TookTooLong = new MessageEmbed()
+  .setColor('#eb4034')
+  .setDescription("You took too long to respond!")
+
+
+async function determineType(method,message,BotMsg,args) {
+  if (isNaN(Number(args[1]))) {
+    byUser(method,args[1],BotMsg);
+  } else {
+    const Emb = new MessageEmbed()
+      .setColor('#ea00ff')
+      //.setTitle(request.headers.username + "'s Data")
+      .setTitle("Is this a UserID or a Username?")
+      //.setAuthor('Roblox Error','')
+      .setDescription("Please react with the number that matches the answer.")
+      .addField(numbers[0] + ": Username","This is a players username in game.")
+      .addField(numbers[1] + ": UserID","This is the players UserId connected with the account.")
+      .setTimestamp()
+      .setFooter('Developed by Stratiz');
+    BotMsg.edit(Emb);
+    try {
+      await BotMsg.react(numbers[0]);
+      await BotMsg.react(numbers[1]);
+    } catch (error) {
+      console.error('One of the emojis failed to react.');
+    }
+    const filter = (reaction, user) => {
+      return numbers.includes(reaction.emoji.name) && user.id === message.author.id;
+    };
+    BotMsg.awaitReactions(filter, { max: 1, time: 30000, errors: ['time'] })
+      .then(collected => {
+        const reaction = collected.first();
+        const ind = numbers.findIndex(function(n){
+           return n == reaction.emoji.name;
+        })
+        BotMsg.reactions.removeAll().catch(error => console.error('Failed to clear reactions: ', error));
+        if (ind == 0) {
+          byUser(method,args[1],BotMsg);
+        } else if (ind == 1) {
+          byUID(method,args[1],BotMsg);
+        } else {
+          BotMsg.edit('Something went wrong');
+        }//
+      })
+      .catch(collected => {
+        BotMsg.edit(TookTooLong);
+        BotMsg.reactions.removeAll().catch(error => console.error('Failed to clear reactions: ', error));
+      });
+  }
+}
+
+app.get('/', async function(request, response) {
+    if (request.headers.username != undefined) { 
+      const channel = await client.channels.cache.get(request.headers.cid);
+      channel.messages.fetch(request.headers.mid)
+        .then(msg => {
+          if (request.headers.rblxerror == undefined) {
+            const Emb = new MessageEmbed()
+                  .setColor('#00ff44')
+                  .setTitle(request.headers.method + " successful. ")
+                  .addField('Username',request.headers.username)
+                  .addField('UserID',request.headers.value)
+                  //.addField('Inline field title', 'Some value here', true)
+                  //.setImage('https://www.roblox.com/Thumbs/Avatar.ashx?x=100&y=100&userId='+request.headers.uid)
+                  .setTimestamp()
+                  .setFooter('Developed by Stratiz');
+            if (msg.author != undefined) {
+              msg.edit(Emb);
+            } else {
+              channel.send(Emb);
+            }
+          } else {
+            const Emb = new MessageEmbed()
+                  .setColor('#eb4034')
+                  .setTitle(request.headers.method + " failed. ")
+                  .addField('Username',request.headers.username)
+                  .addField('UserID',request.headers.value)
+                  .addField('Rblx-Error',request.headers.rblxerror)
+                  //.addField('Inline field title', 'Some value here', true)
+                  //.setImage('https://www.roblox.com/Thumbs/Avatar.ashx?x=100&y=100&userId='+request.headers.uid)
+                  .setTimestamp()
+                  .setFooter('Developed by Stratiz');
+                if (msg.author != undefined) {
+                  msg.edit(Emb);
+                } else {
+                  channel.send(Emb);
+                }
+          }
+      })
+      .catch( err =>{
+        console.log(err);       
+      });
+    }
+    response.send(toGet[0]);
+    toGet.shift();
+});
+
+let listener = app.listen(process.env.PORT, function() {
+    //setInterval(() => { // Used to work sometime ago
+    //    http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+    //}, 280000);
+    console.log('Not that it matters but your app is listening on port ' + listener.address().port);
+});
 
 module.exports = {
     name: 'Checkxp',
@@ -42,6 +199,10 @@ module.exports = {
         //return console.log(interaction.commandId)
         const username = interaction.options.getString('username');
         await interaction.deferReply({ ephemeral: true });
+        var BotMsg = await message.channel.send("<@" + message.author.id + ">",Emb);
+        determineType("Ban",message,BotMsg,args);
+
+        app.use(express.static('public'));
 
         try {
             const id = await Roblox.getIdFromUsername(username)
